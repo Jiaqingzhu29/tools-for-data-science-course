1dc34f77af5873f458a46b2fbaab2e34013c33d6
Jiaqingzhu29 <79582614+Jiaqingzhu29@users.noreply.github.com>
2021-04-07 17:28:33 +0800

    Update python.py

--- 2021spring/2020310666/python.py
+++ 2021spring/2020310666/python.py
@@ -8,3 +8,20 @@ def leap year check(n):
     else:
          print(year, "不是闰年")
     return(I'm done)
+
+           
+           n=int(input("需要第几项\n"))
+a=0
+b=1
+i=2
+if n==1:
+    print(a)
+elif n==2:
+    print(b)
+else:
+    while(i<n):
+        result=a+b
+        a=b
+        b=result
+        i+=1
+    print(result)
